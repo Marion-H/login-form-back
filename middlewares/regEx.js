22b65d318d7bbda1d0570e544cf57fb5e3ac59ac
Marion-H <doudou6500@gmail.com>
2021-02-22 14:35:56 +0100

    add 2 regex for password

--- middlewares/regEx.js
+++ middlewares/regEx.js
@@ -1,2 +1,5 @@
-module.exports.uuidv4RegExp = /[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}/i;
+module.exports.uuidv4RegEx = /[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}/i;
 // module.exports.urlImgRegExp = /(https:)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/;
+module.exports.mobileRegEx = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/i;
+module.exports.strongRegExPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/i;
+module.exports.mediumRegExPwd = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/i
