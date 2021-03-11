2b434c03fcf59781dcd488c6856b03865e3dbd2b
Marion-H <doudou6500@gmail.com>
2021-03-11 15:27:55 +0100

    update varial req body newPassword

--- routes/reset.route.js
+++ routes/reset.route.js
@@ -6,7 +6,7 @@ const resetApp = express.Router();
 const User = require("../models/User");
 
 resetApp.post("/:resetPasswordToken", async (req, res) => {
-  const { password } = req.body;
+  const { newPassword } = req.body;
   const { resetPasswordToken } = req.params;
   try {
     const userFind = await User.scope("passwordActive").findOne({
@@ -20,7 +20,7 @@ resetApp.post("/:resetPasswordToken", async (req, res) => {
         .status(401)
         .json({ message: "Password reset token is invalid or has expired." });
     } else {
-      userFind.password = bcrypt.hashSync(password, bcrypt.genSaltSync());
+      userFind.password = bcrypt.hashSync(newPassword, bcrypt.genSaltSync());
       userFind.resetPasswordToken = null;
       userFind.resetPasswordExpires = null;
       await userFind.save();
